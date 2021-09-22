3552e3fb628b0343a063256e10adf4d105d66f0b
Solomon Baarda <solomon_baarda@outlook.com>
2021-09-22 10:40:01 +0100

    Added basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
